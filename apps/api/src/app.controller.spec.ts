59623071f0870518793f9a8e5a1f4a0e4a7b215e
Moe <mnowicki091@gmail.com>
2025-12-26 18:24:24 -0500

    remove test req from shared types package. Add simple prisma mocks to tests

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -1,14 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
+    const prismaServiceMock = {
+      message: {
+        findFirst: jest.fn(),
+      },
+      $queryRaw: jest.fn(),
+    };
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: PrismaService,
+          useValue: prismaServiceMock,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
--- apps/api/src/auth/auth.service.spec.ts
+++ apps/api/src/auth/auth.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    const prismaServiceMock = {
+      oAuthAccount: {
+        findUnique: jest.fn(),
+        create: jest.fn(),
+      },
+      user: {
+        create: jest.fn(),
+        findUnique: jest.fn(),
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: PrismaService,
+          useValue: prismaServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
